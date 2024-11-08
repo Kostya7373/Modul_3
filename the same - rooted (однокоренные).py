9e7d6c3df833688f9f9e8d2ed4ebf60c887cf6b9
Kostya7373 <kot.gorobey@gmail.com>
2024-11-08 23:54:31 +0300

    Add files via upload

---
+++ the same - rooted (однокоренные).py
@@ -0,0 +1,16 @@
+def single_root_words ( root_word , *other_words) :
+
+    same_words = []
+    for i in range(len(other_words)):
+        a = root_word.lower()
+        b = other_words[i].lower()
+        if a in b or b in a :
+           same_words.append(other_words[i])
+    return (same_words)
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+#['richiest', 'orichalcum', 'richies']
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+#['Able', 'Disable']
+print(result1)
+print(result2)
